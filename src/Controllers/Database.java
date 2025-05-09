b927c87a74704f729b4102e6b497fd98c0c04de1
Caglar Cetinceviz <caglarcetinceviz@gmail.com>
2025-05-09 10:41:03 +0100

    first commit

---
+++ src/Controllers/Database.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+public class Database {
+
+    public static final String host = "127.0.0.1";
+    
+    public static final String port = "3306";
+    
+    public static final String databaseName = "grade_record_system";
+
+    public static final String userName = "caglar";
+    
+    public static final String password = "123456";
+    
+}
